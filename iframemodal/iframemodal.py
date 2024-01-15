5ed4c5ff5718eed6b3cdb03d40072f1d50bbb35a
Ridnarong Promya <ridnarong@hotmail.com>
2024-01-15 23:18:31 +0700

    iframe

--- iframemodal/iframemodal.py
+++ iframemodal/iframemodal.py
@@ -1,76 +1,55 @@
 """TO-DO: Write a description of what this XBlock is."""
 
 import pkg_resources
-from lti_consumer import LtiConsumerXBlock
-from lti_consumer.utils import _
 from web_fragments.fragment import Fragment
 from xblock.core import XBlock
 from xblock.fields import String, Scope
-from xblockutils.resources import ResourceLoader
 
-class IFrameModalXBlock(LtiConsumerXBlock):
-    override_launch_url = String(
-        display_name=_("Launch URL"),
-        default='https://applications.zoom.us/lti/rich',
-        scope=Scope.settings
-    )
 
-    lti_id = 'zoom'
-    display_name = String(
-        display_name=_("Display Name"),
-        help=_(
-            "Enter the name that students see for this component. "
-            "Analytics reports may also use the display name to identify this component."
-        ),
-        scope=Scope.settings,
-        default=_("Zoom"),
-    )
-    description = String(
-        display_name=_("Application Information"),
-        help=_(
-            "Enter a description of your use of Zoom. "
-        ),
-        default=_("Use Zoom to host office hours and other course meetings"),
-        scope=Scope.settings
-    )
-    block_settings_key = 'edx_zoom'
+class IFrameModalXBlock(XBlock):
+    """
+    TO-DO: document what your XBlock does.
+    """
 
-    editable_fields = (
-        'display_name', 'description', 'custom_parameters', 'override_launch_url',
-        'inline_height', 'modal_height', 'modal_width'
+    # Fields are defined on the class.  You can access them in your code as
+    # self.<fieldname>.
+
+    # TO-DO: delete count, and define your own fields.
+    ifrome_url = String(
+        default='', scope=Scope.user_state,
+        help="iframe URL",
     )
-    ask_to_send_username = ask_to_send_email = True
-    has_author_view = True
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
 
-    def _get_context_for_template(self):
-        ctx = super()._get_context_for_template()
-        ctx['missing_credentials'] = self.lti_provider_key_secret[0] is None
-        ctx['ask_to_send_username'] = ctx['ask_to_send_email'] = True
-        return ctx
-
     # TO-DO: change this view to display your data your own way.
     def student_view(self, context=None):
-        fragment = Fragment()
-        lti_loader = ResourceLoader('lti_consumer')
-        loader = ResourceLoader(__name__)
-        context = self._get_context_for_template()
-        fragment.add_content(loader.render_mako_template('/templates/student.html', context))
-        fragment.add_css(loader.load_unicode('static/css/student.css'))
-        fragment.add_javascript(lti_loader.load_unicode('static/js/xblock_lti_consumer.js'))
-        fragment.initialize_js('LtiConsumerXBlock')
-        return fragment
+        """
+        The primary view of the IFrameModalXBlock, shown to students
+        when viewing courses.
+        """
+        html = self.resource_string("static/html/iframemodal.html")
+        frag = Fragment(html.format(self=self))
+        frag.add_css(self.resource_string("static/css/iframemodal.css"))
+        frag.add_javascript(self.resource_string("static/js/src/iframemodal.js"))
+        frag.initialize_js('IFrameModalXBlock')
+        return frag
 
-    def author_view(self, context):
-        fragment = Fragment()
-        loader = ResourceLoader(__name__)
-        context.update(self._get_context_for_template())
-        fragment.add_content(loader.render_mako_template('/templates/author.html', context))
-        return fragment
+    # TO-DO: change this handler to perform your own actions.  You may need more
+    # than one handler, or you may not need any handlers at all.
+    @XBlock.json_handler
+    def increment_count(self, data, suffix=''):
+        """
+        An example handler, which increments the data.
+        """
+        # Just to show data coming in...
+        assert data['hello'] == 'world'
+
+        self.count += 1
+        return {"count": self.count}
 
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
--- iframemodal/static/js/src/iframemodal.js
+++ iframemodal/static/js/src/iframemodal.js
@@ -7,16 +7,43 @@ function IFrameModalXBlock(runtime, element) {
 
     var handlerUrl = runtime.handlerUrl(element, 'increment_count');
 
-    $('p', element).click(function(eventObject) {
-        $.ajax({
-            type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify({"hello": "world"}),
-            success: updateCount
-        });
+    $('button[data-target]', element).click(function(eventObject) {
+        console.log(eventObject);
+        const overlay = $("<div id='my-iframe-modal_lean-overlay' class='lean-overlay'></div>");
+        $("body").append(overlay);
+        if (window !== window.parent) {
+          window.parent.postMessage(
+              {
+                  'type': 'plugin.modal',
+                  'payload': {
+                      'url': 'https://www.google.com',
+                      'title': 'title',
+                      'width': '400px'
+                  }
+              },
+              document.referrer
+          );
+          return;
+        }
     });
 
     $(function ($) {
         /* Here's where you'd do things on page load. */
+        // $.fn.extend({
+        //   iframeModal: function (options) {
+        //     var $trigger = $(this);
+        //     var modal_id = $trigger.data("target");
+        //     var defaults = { top: 100, overlay: 0.5, closeButton: null };
+        //     var overlay_id = (modal_id + '_lean-overlay').replace('#', '');
+        //     var overlay = $("<div id='" + overlay_id + "' class='lean-overlay'></div>");
+        //     $("body").append(overlay);
+        //     return this.each(function () {
+        //       var o = options;
+
+        //       var $modal = $(modal_id);
+        //       return $modal;
+        //     })
+        //   }
+        // })
     });
 }
