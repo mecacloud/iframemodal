4e746b42d69b0105a33718eaadf8584c156251c4
Ridnarong Promya <ridnarong@hotmail.com>
2024-01-16 11:00:08 +0700

    js modal

--- iframemodal/static/js/src/iframemodal.js
+++ iframemodal/static/js/src/iframemodal.js
@@ -22,23 +22,44 @@ function IFrameModalXBlock(runtime, element) {
     });
 
     $(function ($) {
-      
-        /* Here's where you'd do things on page load. */
-        // $.fn.extend({
-        //   iframeModal: function (options) {
-        //     var $trigger = $(this);
-        //     var modal_id = $trigger.data("target");
-        //     var defaults = { top: 100, overlay: 0.5, closeButton: null };
-        //     var overlay_id = (modal_id + '_lean-overlay').replace('#', '');
-        //     var overlay = $("<div id='" + overlay_id + "' class='lean-overlay'></div>");
-        //     $("body").append(overlay);
-        //     return this.each(function () {
-        //       var o = options;
-
-        //       var $modal = $(modal_id);
-        //       return $modal;
-        //     })
-        //   }
-        // })
+      function openModal($el) {
+        $el.classList.add('is-active');
+      }
+    
+      function closeModal($el) {
+        $el.classList.remove('is-active');
+      }
+    
+      function closeAllModals() {
+        (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+          closeModal($modal);
+        });
+      }
+    
+      // Add a click event on buttons to open a specific modal
+      (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+    
+        $trigger.addEventListener('click', () => {
+          openModal($target);
+        });
+      });
+    
+      // Add a click event on various child elements to close the parent modal
+      (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+        const $target = $close.closest('.modal');
+    
+        $close.addEventListener('click', () => {
+          closeModal($target);
+        });
+      });
+    
+      // Add a keyboard event to close all modals
+      document.addEventListener('keydown', (event) => {
+        if (event.code === 'Escape') {
+          closeAllModals();
+        }
+      });
     });
 }
